1967e2f280406f699ee1e73b2f1cd2fe7813b90f
Lazare Fortune <lazarefortune@gmail.com>
2024-03-29 21:23:21 +0100

    chore: update home page title

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import { CourseListServer } from './course-list.server';
+import Head from "next/head";
 
+export const metadata = {
+    title: 'Accueil - FlexiLearn',
+}
 export default function Home() {
     return (
-        <main className="min-h-screen max-w-5xl mx-auto">
-            <h1 className="text-4xl text-slate-900 font-bold text-center mt-10">Welcome to FlexiLearn</h1>
-            <p className="text-lg text-slate-500 text-center mb-10">Des cours adaptés entièrement à vos besoins, à votre rythme, et à votre niveau.</p>
-            <CourseListServer />
-        </main>
+        <>
+            <main className="min-h-screen max-w-5xl mx-auto">
+                <h1 className="text-4xl text-slate-900 font-bold text-center mt-10">Welcome to FlexiLearn</h1>
+                <p className="text-lg text-slate-500 text-center mb-10">Des cours adaptés entièrement à vos besoins, à votre rythme, et à votre niveau.</p>
+                <CourseListServer />
+            </main>
+        </>
     );
 }
